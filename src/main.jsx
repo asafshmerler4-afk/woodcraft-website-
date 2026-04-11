b89382994a72434135e74fa8e8965e85b155c462
asafshmerler4-afk <asafshmerler4@gmail.com>
2026-04-12 00:37:13 +0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Navbar from './Navbar.js' // או .jsx, תלוי איך קראת להם בתיקייה
-import KitsSection from './KitsSection.js'
-import WorkshopsSection from './WorkshopsSection.js'
-import Footer from './Footer.js'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
-const App = () => {
-  return (
-    <div className="min-h-screen bg-white text-right" dir="rtl">
-      <Navbar />
-      <main>
-        <KitsSection />
-        <WorkshopsSection />
-      </main>
-      <Footer />
-    </div>
-  )
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
